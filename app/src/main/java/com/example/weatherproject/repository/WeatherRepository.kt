11c99ddc16aaa30b2396abd2fa54bb4cfe4e787d
Sagar727 <sagar727@gmail.com>
2023-11-11 15:12:43 -0500

    added weather repository, retrofit

---
+++ app/src/main/java/com/example/weatherproject/repository/WeatherRepository.kt
@@ -0,0 +1,30 @@
+package com.example.weatherproject.repository
+
+import androidx.lifecycle.MutableLiveData
+import com.example.weather.test.WeatherData
+import com.example.weatherproject.services.WeatherService
+import retrofit2.Retrofit
+
+
+class WeatherRepository : IWeatherRepository {
+
+    override suspend fun getWeatherData(
+        retrofit: Retrofit,
+        weatherLiveData: MutableLiveData<WeatherData>,
+        key: String,
+        loc: String,
+    ) {
+        val weatherService = retrofit.create(WeatherService::class.java)
+        val weather = weatherService.getWeather(key, loc, 3)
+        weatherLiveData.value = weather
+    }
+}
+
+interface IWeatherRepository {
+    suspend fun getWeatherData(
+        retrofit: Retrofit,
+        weatherLiveData: MutableLiveData<WeatherData>,
+        key: String,
+        loc: String,
+    )
+}
